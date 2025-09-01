99d1bb7bf281aee39095f7da0a1b424fbf463c51
Pankaj kandpal <pankajkandpal7500@gmail.com>
2025-09-01 20:41:07 +0530

    new change

--- src/main/java/generic_utility/FileUtility.java
+++ src/main/java/generic_utility/FileUtility.java
@@ -13,33 +13,36 @@ public class FileUtility {
 	public String getDataFromProperties(String key) throws IOException {
 		FileInputStream fis = new FileInputStream(
 				"C:\\Users\\HP\\Pictures\\javaprograms\\SeleniumProjectWinners\\my-testcases\\src\\test\\resource\\commomdata.properties");
-		
+
 		Properties pobj = new Properties();
 		pobj.load(fis);
 		// step 3> get the value by passing the keys
-				String BROWSER = pobj.getProperty("bro");
-				String URL = pobj.getProperty("url");
-				String USERNAME = pobj.getProperty("un");
-				String PASSWORD = pobj.getProperty("pwd");
+		String BROWSER = pobj.getProperty("bro");
+		String URL = pobj.getProperty("url");
+		String USERNAME = pobj.getProperty("un");
+		String PASSWORD = pobj.getProperty("pwd");
 		String value = pobj.getProperty(key);
 		return value;
 	}
 
-	public String  getDataFromexcelFile(String sheetName, int rowNum, int cellNum) throws EncryptedDocumentException, IOException {
+	public String getDataFromexcelFile(String sheetName, int rowNum, int cellNum)
+			throws EncryptedDocumentException, IOException {
 		FileInputStream fis1 = new FileInputStream(
 				"C:\\Users\\HP\\Pictures\\javaprograms\\SeleniumProjectWinners\\my-testcases\\src\\test\\resource\\TestOne.xlsx");
 		Workbook wb = WorkbookFactory.create(fis1);
-		Sheet  sh = wb.getSheet("Sheet1");
-		String  value=sh.getRow(rowNum).getCell(cellNum).getStringCellValue();
-		return value;			
+		Sheet sh = wb.getSheet("Sheet1");
+		String value = sh.getRow(rowNum).getCell(cellNum).getStringCellValue();
+		return value;
 	}
-	public int getNumericDataFromExcelFile(String sheetName, int rowNum, int cellNum) throws EncryptedDocumentException, IOException {
+
+	public int getNumericDataFromExcelFile(String sheetName, int rowNum, int cellNum)
+			throws EncryptedDocumentException, IOException {
 		FileInputStream fis1 = new FileInputStream(
 				"C:\\Users\\HP\\Pictures\\javaprograms\\SeleniumProjectWinners\\my-testcases\\src\\test\\resource\\TestOne.xlsx");
 		Workbook wb = WorkbookFactory.create(fis1);
-		Sheet  sh = wb.getSheet("Sheet1");
+		Sheet sh = wb.getSheet("Sheet1");
 		int value = (int) sh.getRow(rowNum).getCell(cellNum).getNumericCellValue();
-		return value;	
-		
+		return value;
+
 	}
 }
